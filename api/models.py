614860be3c70172209d3d04cbfdeca2a26d57781
oxenprogrammer <emmy1000okello@gmail.com>
2018-08-14 15:23:37 +0300

    adds a list of dictionaries to hold the data[#159761405 Fixes]

---
+++ api/models.py
@@ -0,0 +1,24 @@
+'''Data Structure to hold non persistent data'''
+
+questions = [{
+    'question_id': 1,
+    'question_class': 'python',
+    'question_body': 'what is python?',
+    'answer': [
+        {'answer_id': 1,
+         'answer_body': 'Python is an interpreted high-level \
+            programming language for general-purpose programming.'},
+        {'answer_id': 2,
+         'answer_body': 'Python is an interpreted high-level \
+            programming language for general-purpose programming.\
+            Created by Guido van Rossum and first released in 1991'}]
+},
+    {
+    'question_id': 2,
+    'question_class': 'javascript',
+    'question_body': 'what is javascript?',
+    'answer': [
+        {'answer_id': '',
+         'answer_body': ''
+         }]
+}]
