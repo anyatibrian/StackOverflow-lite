ce9b7b648a705332ae6b246b25fcc515fb66ee17
oxenprogrammer <emmy1000okello@gmail.com>
2018-08-16 16:14:45 +0300

    tests get all questions endpoint [#159761465 Fixes]

---
+++ tests/tests.py
@@ -0,0 +1,31 @@
+from api import views, models
+import app
+from copy import deepcopy
+import json
+import unittest
+
+
+DEFAULT_URL = 'http://127.0.0.1:5000/api/v1/questions'
+BAD_ITEM_URL = '{}/3'.format(DEFAULT_URL)
+GOOD_ITEM_URL = '{}/2'.format(DEFAULT_URL)
+
+
+class TestApi(unittest.TestCase):
+    '''Test the api end points'''
+    def setUp(self):
+        self.questionCopy = deepcopy(views.questions)
+        self.answerCopy = deepcopy(views.answers)
+        self.app = app.app.test_client()
+        self.app.testing = True
+
+    def test_get_all_questions(self):
+        res = self.app.get(DEFAULT_URL)
+        data = json.loads(res.get_data())
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(len(data['questions']), 2)
+
+    def tearDown(self):
+        '''reset views.questions and views.answers to initial state
+        '''
+        views.questions = self.questionCopy
+        views.answers = self.answerCopy
